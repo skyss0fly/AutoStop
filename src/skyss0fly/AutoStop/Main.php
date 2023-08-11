21d7fd8b98d1eb4da9cc80b0c9bccc9fae0ec93f
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-11 22:00:15 +1000

    Update and rename config.yml to Main.php

--- src/skyss0fly/AutoStop/Main.php
+++ src/skyss0fly/AutoStop/Main.php
@@ -9,8 +9,15 @@ class Main extends PluginBase {
 public function onLoad(): void {
 $this->saveDefaultConfig();
 $cfg = $this->getConfig();
-
+$this->time();
 
 
 }
+  public function time() {
+$timenow = $this->DateTime()->TimeNow();
+    $cfg = $this->getConfig();
+
+    sleep($cfg * 60)
+    $this->getServer()->stopServer();
+  }
 }
